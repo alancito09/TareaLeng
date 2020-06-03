62567b02de064b80b637d69b50f74ea9140af6d1
alancito09 <65987486+alancito09@users.noreply.github.com>
2020-06-02 18:49:56 -0600

    arreglado
    ulti

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 <h1>Ejercicio de PHP</h1>
 
 <ul>
-    <li><a href="Ejercicio#2.php" target="_new">Ejercicio 2</a></li>
-    <li><a href="Ejercicio#3.php" target="_new">Ejercicio 3</a></li>
-    <li><a href="Ejercicio#4.php" target="_new">Ejercicio 4</a></li>
-    <li><a href="Ejercicio#5.php" target="_new">Ejercicio 5</a></li>
-    <li><a href="Ejercicio#6.php" target="_new">Ejercicio 6</a></li>
-    <li><a href="Ejercicio#7.php?clase=Lenguaje 4&periodo=2020-01" target="_new">Ejercicio 7</a></li>
+    <li><a href="Ejercicio_2.php" target="_new">Ejercicio 2</a></li>
+    <li><a href="Ejercicio_3.php" target="_new">Ejercicio 3</a></li>
+    <li><a href="Ejercicio_4.php" target="_new">Ejercicio 4</a></li>
+    <li><a href="Ejercicio_5.php" target="_new">Ejercicio 5</a></li>
+    <li><a href="Ejercicio_6.php" target="_new">Ejercicio 6</a></li>
+    <li><a href="Ejercicio_7.php?clase=Lenguaje 4&periodo=2020-01" target="_new">Ejercicio 7</a></li>
 
 </ul>
 
